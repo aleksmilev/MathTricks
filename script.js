cbdfb2eb1a0b2da95356feb0b77c5e37b4a11258
aleksmilev <72939240+aleksmilev@users.noreply.github.com>
2023-06-09 23:20:08 +0300

    Creating basic HTML, CSS and JS

---
+++ script.js
@@ -0,0 +1,91 @@
+class Player{
+    x_position;
+    y_position;
+
+    constructor(position){
+        this.x_position = position[0];
+        this.y_position = position[1];
+    }
+
+    move_player(new_position){
+        this.x_position = new_position[0];
+        this.y_position = new_position[1];
+    }
+
+    get_available_moves(){
+        let available_moves = new Array();
+        let posible_moves = [[-1, 0, 1], [-1, 0, 1]];
+
+        for(let i = 0; i < posible_moves.length; i++){
+            for(let j = 0; j < posible_moves[i].length; j++){
+                // if(){
+                    
+                // }
+            }
+        }
+    }
+}
+
+class Node{
+    x_axis;
+    y_axis;
+    value;
+
+    owner = null;
+    
+    constructor(position, value){
+        this.x_axis = position[0];
+        this.y_axis = position[1];
+
+        this.value = value;
+    }
+
+    set_owner(new_owner){
+        this.owner = new_owner;
+    }
+}
+
+class Grid{
+    static grid_feald = new Array();
+
+    static create_node_row(){
+        const node_row = document.createElement("div");
+        node_row.classList.add("node_row");
+        document.getElementById("game").appendChild(node_row);
+
+        return node_row;
+    }
+    static create_node(node, node_row){
+        const node_el = document.createElement("h1");
+        node_el.classList.add("node_el");
+        node_el.innerText = node.value;
+
+        node_row.appendChild(node_el);
+    }
+
+    x_axis;
+    y_axis;
+
+    constructor(dimensions){
+        this.x_axis = dimensions[0];
+        this.y_axis = dimensions[1];
+
+        for(let i = 0; i < dimensions[0]; i++){
+            Grid.grid_feald.push(new Array());
+
+            for(let j = 0; j < dimensions[1]; j++){
+                Grid.grid_feald[i].push(new Node([dimensions[0], dimensions[1]], "test"));
+            }
+        }
+    }
+}
+
+const a = new Grid([5, 5]);
+
+Grid.grid_feald.forEach(node_row => {
+    let curent_row = Grid.create_node_row();
+
+    node_row.forEach(node_el => {
+        Grid.create_node(node_el, curent_row);
+    })
+});
